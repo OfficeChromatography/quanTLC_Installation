1a5a409ae433b26c1163094c3ead198b737216ff
wschwack <wolfgang.schwack@uni-giessen.de>
2023-06-12 14:22:38 +0200

    modified

--- shiny-server-2.sh
+++ shiny-server-2.sh
@@ -15,19 +15,20 @@ then
   echo ""
   yes | sudo apt install gdebi-core
   yes | wget https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-1.5.20.1002-amd64.deb
-  yes | sudo gdebi shiny-server-1.5.20.1002-amd64.deb
+  #yes | sudo gdebi shiny-server-1.5.20.1002-amd64.deb  #funktioniert nicht!! Manuell ausführen!!
   echo ""
   echo "Copying quanTLC to Shiny Server."
-  yes | sudo cp -R /usr/local/lib/R/site-library/quanTLC/shinyapps/quanTLC /srv/shiny-server
+  yes | sudo cp -R /usr/local/lib/R/site-library/quanTLC/shinyapps/quanTLC /srv/shiny-server/quanTLC
   yes | sudo chmod 777 -R /srv/shiny-server/quanTLC
   echo ""
   echo "Downloading rTLC"
   yes | sudo git clone https://github.com/OfficeChromatography/rTLC-apps.git
-  yes | sudo cp -R rTLC-apps /srv/shiny-server
+  yes | sudo cp -R rTLC-apps /srv/shiny-server/rTLC-apps
   yes | sudo mv /srv/shiny-server/rTLC-apps /srv/shiny-server/rTLC
   echo ""
-  echo "Performing reboot"
-  sudo reboot
+  #echo "Performing reboot"
+  #sudo reboot
+  echo "Shiny-Server manuell installieren!!"
 else
   echo "The installation was skipped"
 fi
