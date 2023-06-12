90c5bbfa6b21ae72bc655060c8869a1716021852
wschwack <wolfgang.schwack@uni-giessen.de>
2023-06-12 12:15:41 +0200

    Files added

---
+++ shiny-server-1.sh
@@ -0,0 +1,48 @@
+#install script for Shiny-Server: quanTLC and rTLC _ Part 1
+#!/bin/bash
+echo""
+echo "***********************************************************"
+echo ""
+echo "You are going to install quanTLC."
+echo ""
+echo "Don't worry, this will take some time!"
+echo ""
+echo "Are you ready to start? (y/n)"
+read userinput
+if [ "$userinput" == "y" ]
+then
+  echo "Installing r-base"
+  yes | sudo apt install r-base r-base-dev     #--version=4.1.2??
+  echo ""
+  echo "Installing libraries"
+  yes | sudo apt install libssl-dev libcurl4-openssl-dev r-cran-rgl libtiff5-dev 
+  yes | sudo apt install libssh2-1-dev libxml2-dev libgit2-dev libnlopt-dev
+  echo ""
+  echo "Installing Java"
+  yes | sudo apt install default-jdk
+  echo "Installing LaTex"
+  yes | sudo apt install texlive-latex-extra
+  echo ""
+  echo "Installing R packages"
+  echo ""
+  yes | sudo su - -c "R -e \"install.packages('devtools')\""
+  yes | sudo su - -c "R -e \"install.packages('rJava',,'http://rforge.net', type='source')\""
+  yes | sudo su - -c "R -e \"library(devtools)\""
+  yes | sudo su - -c "R -e \"devtools::install_github('colearendt/xlsx')\""
+  yes | sudo su - -c "R -e \"devtools::install_github('cran/DiscriMiner')\""
+  yes | sudo su - -c "R -e \"devtools::install_github('cran/randomForest')\""     #benötigt R > 4.1.0 
+  yes | sudo su - -c "R -e \"devtools::install_github('cran/extraTrees')\""
+  yes | sudo su - -c "R -e \"devtools::install_github('rstudio/d3heatmap')\""
+  yes | sudo su - -c "R -e \"devtools::install_github('rwehrens/ChemometricsWithR')\""
+  yes | sudo su - -c "R -e \"install.packages('shiny', repos='https://cran.rstudio.com/')\""
+  yes | sudo su - -c "R -e \"tinytex::install_tinytex()\""
+  yes | sudo su - -c "R -e \"install.packages('remotes', repos='https://cran.rstudio.com/')\""
+  yes | sudo su - -c "R -e \"library(remotes)\""
+  yes | sudo su - -c "R -e \"remotes::install_github('OfficeChromatography/quanTLC', lib='/usr/local/lib/R/site-library/')\""
+  yes | sudo chmod 777 -R /usr/local/lib/R/site-library/quanTLC/shinyapps/quanTLC
+  echo ""
+  echo "Performing reboot"
+  sudo reboot
+else
+  echo "The installation was skipped"
+fi
---
+++ shiny-server-2.sh
@@ -0,0 +1,33 @@
+#install script for Shiny-Server: quanTLC and rTLC _ Part 2
+#!/bin/bash
+echo""
+echo "***********************************************************"
+echo ""
+echo "You are going to install shiny-server."
+echo ""
+echo "Don't worry, this will take some time!"
+echo ""
+echo "Are you ready to start? (y/n)"
+read userinput
+if [ "$userinput" == "y" ]
+then
+  echo "Installing Shiny Server"
+  echo ""
+  yes | sudo apt install gdebi-core
+  yes | wget https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-1.5.20.1002-amd64.deb
+  yes | sudo gdebi shiny-server-1.5.20.1002-amd64.deb
+  echo ""
+  echo "Copying quanTLC to Shiny Server."
+  yes | sudo cp -R /usr/local/lib/R/site-library/quanTLC/shinyapps/quanTLC /srv/shiny-server
+  yes | sudo chmod 777 -R /srv/shiny-server/quanTLC
+  echo ""
+  echo "Downloading rTLC"
+  yes | sudo git clone https://github.com/OfficeChromatography/rTLC-apps.git
+  yes | sudo cp -R rTLC-apps /srv/shiny-server
+  yes | sudo mv /srv/shiny-server/rTLC-apps /srv/shiny-server/rTLC
+  echo ""
+  echo "Performing reboot"
+  sudo reboot
+else
+  echo "The installation was skipped"
+fi
